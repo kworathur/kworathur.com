76e40cd52e002212b4a2883e9cc487537d67df66
Keshav Worathur <keshavworathur@gmail.com>
2026-05-09 17:03:52 -0400

    feat: update intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -21,22 +21,36 @@ const Intro = (): ReactElement => {
         <p>Welcome to my website! I'm a software engineer that's:</p>
         <ul>
           <li>
-            Built pre-processing pipelines for messy protein structures{' '}
+            Built pre-processing pipelines for protein structures{' '}
             <a href="">like this one</a>, trained ML models to{' '}
             <a href="">see and understand</a> 3D structure data like a chemist,
             and packaged predictions in to an industry leading drug discovery
-            platform at <a href="https://gene2lead.com/">Gene2Lead, Ltd.</a>
+            platform at{' '}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://gene2lead.com/"
+            >
+              Gene2Lead, Ltd.
+            </a>
           </li>
           <li>
             Currently: developing the software infrastructure to scale{' '}
-            <a href="https://www.matmerize.com/">Matmerize's</a> flagship R&D
-            platform used by 10+ customers for AI-assisted industrial material
-            design.
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.matmerize.com/"
+            >
+              Matmerize's
+            </a>{' '}
+            flagship R&D platform used by 10+ customers for AI-assisted
+            industrial material design.
           </li>
         </ul>
         <p>
-          I earned my BSc. in Computer Science from the University of Toronto,
-          and am currently studying AI and systems at Georgia Tech.
+          My experience spans applied ML and distributed systems, which I've
+          gotten to explore further at Georiga Tech. I'm also a proud alum of
+          the University of Toronto (CS '25)
         </p>
         <p>Hobbies: swimming, public speaking, rock music</p>
       </div>
