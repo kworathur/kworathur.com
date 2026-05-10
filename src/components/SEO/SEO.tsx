241131f258de637d6c7e6d09c0132ea68cf61460
Keshav <77427424+kworathur@users.noreply.github.com>
2026-05-10 12:37:05 -0400

    fix: inaccessible links (#3)

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -1,68 +1,70 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement } from 'react';
 import Helmet from 'react-helmet';
-import { useStaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from 'gatsby';
 
 type Meta =
-    | { name: string; content: string; property?: undefined }
-    | { property: string; content: string; name?: undefined };
+  | { name: string; content: string; property?: undefined }
+  | { property: string; content: string; name?: undefined };
 interface SEOProps {
-    description?: string;
-    lang?: string;
-    meta: Meta[];
-    title: string;
+  description?: string;
+  lang?: string;
+  meta: Meta[];
+  title: string;
 }
 
 const SEO = ({ description, lang, meta, title }: SEOProps): ReactElement => {
-    const { site } = useStaticQuery(
-        graphql`
-            query {
-                site {
-                    siteMetadata {
-                        title
-                        description
-                    }
-                }
-            }
-        `
-    );
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+        }
+      }
+    }
+  `);
 
-    const metaDescription: string =
-        description || site.siteMetadata.description;
+  const metaDescription: string = description || site.siteMetadata.description;
 
-    return (
-        <Helmet
-            htmlAttributes={{
-                lang,
-            }}
-            title={title}
-            titleTemplate={`%s | ${site.siteMetadata.title}`}
-            meta={[
-                {
-                    name: `description`,
-                    content: metaDescription,
-                },
-                {
-                    property: `og:title`,
-                    content: title,
-                },
-                {
-                    property: `og:description`,
-                    content: metaDescription,
-                },
-                {
-                    property: `og:type`,
-                    content: `website`,
-                },
-            
-            ].concat(meta as ConcatArray<Meta>)}
-        />
-    );
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+      ].concat(meta as ConcatArray<Meta>)}
+    >
+      <script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="debd09d3-019b-4a83-98c8-78e3b844df35"
+      ></script>
+    </Helmet>
+  );
 };
 
 SEO.defaultProps = {
-    lang: `en`,
-    meta: [],
-    description: ``,
+  lang: `en`,
+  meta: [],
+  description: ``,
 };
 
 export default SEO;
--- src/components/Socials/Socials.tsx
+++ src/components/Socials/Socials.tsx
@@ -1,19 +1,40 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement } from 'react';
 
-import * as styles from "./Socials.module.scss"
+import * as styles from './Socials.module.scss';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Socials = (): ReactElement => {
-    return (
-        <div className={styles.wrapper}>
-            <a href="https://linkedin.com/in/keshav-w" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} /></a>
-            <a href="https://github.com/kworathur" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="mailto:kworathur3@gatech.edu" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a>
-        </div>
-    )
-}
+  return (
+    <div className={styles.wrapper}>
+      <a
+        href="https://linkedin.com/in/keshav-w"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Link to Keshav's linkedin"
+      >
+        <FontAwesomeIcon icon={faLinkedinIn} />
+      </a>
+      <a
+        href="https://github.com/kworathur"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Link to Keshav's github"
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a
+        href="mailto:kworathur3@gatech.edu"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Email Keshav"
+      >
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+    </div>
+  );
+};
 
-export default Socials
+export default Socials;
